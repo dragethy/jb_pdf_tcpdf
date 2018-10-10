7a144b550457d03cd9d1d1b47c6606bd389a2218
Jon Buckner <jon.buckner@sky.com>
2018-10-10 14:01:23 +0800

    Update stringToArray.php

--- assist-files/stringToArray.php
+++ assist-files/stringToArray.php
@@ -63,12 +63,12 @@ function stringSplitToArray($string)
         
         // assign splitting criteria
         $condition = 'array(';
-        $SplitHere = ')';
-        $remove[0] = 'array('; //  $trim is pretty consistent, so add an extra array for 'specials'
+        $splitter = ')';
         // $trim is pretty consistent, should be function
-        $trim[0] = '"';
+        $trim[0] = 'arrray('; // the remainder trims are same
         $trim[1] = "'";
-        $trim[2] = " \t\n";
+        $trim[2] = " "
+        $trim[3] = " \t\n"
         
     }
     elseif(strpos( $data, ',' ) !== false)
@@ -76,10 +76,11 @@ function stringSplitToArray($string)
         
         // assign splitting criteria
         $condition = ',';
-        $SplitHere = ',';
-        $trim[0] = '"';
+        $splitter = ',';
+        $trim[0] = '';
         $trim[1] = "'";
-        $trim[2] = " \t\n";
+        $trim[2] = " "
+        $trim[3] = " \t\n"
         
     }
     elseif(strpos( $data, '=>' ) !== false)
@@ -87,14 +88,16 @@ function stringSplitToArray($string)
         
         // assign splitting criteria
         $condition = '=>';
-        $SplitHere = '=>';
-        $trim[0] = '"';
+        $splitter = '=>';
+        $trim[0] = '';
         $trim[1] = "'";
-        $trim[2] = " \t\n";
+        $trim[2] = " "
+        $trim[3] = " \t\n"
         
     }
     
     
+    $data = _tcpdfStringSplit($string, $condition, $splitter, $remove, $trim);
     // Now we have the match from above, we can act on it
     // This function will call itself as a way to check each value received
     // i.e. if it encounters an array, ', or =>'
@@ -102,7 +105,7 @@ function stringSplitToArray($string)
     {
         case 'array(':
             // split string at ')', $remove and $trim
-            
+            $split = _tcpdfStringSplit();
             break;
             
         case ',':
@@ -120,6 +123,112 @@ function stringSplitToArray($string)
             break;
     }
   
+}
+
+/**
+ * _tcpdfStringSplit
+ *
+ * pass any remove values
+ *
+ * trim values as array or single value
+ *
+ *
+ **/
+function _tcpdfStringSplit( $string, $condition = '', $splitter, $remove = '', $trim = '' )
+{
+    
+    // if condition, split
+    if(strlen($condition) > 0)
+    {
+
+        $exploded = explode( $splitter, $string, 2);
+        
+        // string to process further
+        $current = $exploded[0];
+        
+        // string to process later
+        $remainder = $exploded[1];
+        
+        // remove $exploded
+        unset($exploded);
+        
+        // now sasign stuff based on $condition
+        switch($condition)
+        {
+            case 'array(':
+                // trim it
+                $current = trim($remove);
+                
+                foreach($trim as $key => $value)
+                {
+                    
+                    $current = trim($current, $value);
+                    
+                }
+                
+                
+                $current = trim();
+                $current = trim();
+                break;
+                
+            case ',':
+                //
+                
+                break;
+    
+            case '=>':
+                //
+                
+                break;
+    
+            default:
+                
+                break;
+        }
+    }
+    else
+    {
+        
+        // string to process later
+        $remainder = '';
+        
+        // string to process further
+        $current = $string;
+        
+    }
+    
+    
+    
+/**
+ * _tcpdfTrim
+ *
+ * pass any remove values
+ *
+ * trim values as array or single value
+ *
+ *
+ **/
+function _tcpdfTrim( $string, $trim = '' )
+{
+    // clean it up
+    if((strlen($trim) > 0) || is_array($trim))
+    {
+        
+        foreach($trim as $key => $value)
+        {
+            
+            if(is_string($key))
+            {
+                
+                // trim it, baby
+                $key = trim();
+            }
+        }
+    
+    }
+    
+    
+    
     
 
 }
